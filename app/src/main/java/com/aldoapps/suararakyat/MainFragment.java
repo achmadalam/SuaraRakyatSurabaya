ce0d5856b3931173fd82a15b0c6d07caf35685e2
aldokelvianto <aldokelvianto@yahoo.com>
2015-11-04 18:22:01 +0700

    add icon to fab

--- app/src/main/java/com/aldoapps/suararakyat/MainFragment.java
+++ app/src/main/java/com/aldoapps/suararakyat/MainFragment.java
@@ -11,7 +11,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
-import com.facebook.drawee.view.SimpleDraweeView;
 import com.getbase.floatingactionbutton.FloatingActionButton;
 import com.getbase.floatingactionbutton.FloatingActionsMenu;
 
@@ -27,7 +26,7 @@ public class MainFragment extends Fragment {
 
     private static final int SPEECH_REQUEST_CODE = 1692;
 
-    @Bind(R.id.vote_by_dialog) FloatingActionButton mDialogBtn;
+    @Bind(R.id.vote_by_manual) FloatingActionButton mManualBtn;
     @Bind(R.id.vote_by_voice) FloatingActionButton mVoiceBtn;
     @Bind(R.id.fab_menu) FloatingActionsMenu mFabMenu;
     @Bind(R.id.candidate_one) ImageView mCandidateOneImg;
@@ -47,9 +46,10 @@ public class MainFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.main_fragment, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
         ButterKnife.bind(this, view);
 
+        mVoiceBtn.setIcon(R.drawable.ic_action_microphone);
         mVoiceBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -61,6 +61,8 @@ public class MainFragment extends Fragment {
                 startActivityForResult(intent, SPEECH_REQUEST_CODE);
             }
         });
+
+        mManualBtn.setIcon(R.drawable.ic_action_pin);
 
         return view;
     }
